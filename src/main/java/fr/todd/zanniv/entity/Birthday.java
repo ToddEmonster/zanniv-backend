cc4edbb7890eb178fdad3312b2d50b27c2bd2da1
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-08-22 16:47:32 +0200

    feat: add models

---
+++ src/main/java/fr/todd/zanniv/entity/Birthday.java
@@ -0,0 +1,59 @@
+package fr.todd.zanniv.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Birthday")
+public class Birthday {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="email")
+    private String email;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Birthday() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String toString() {
+        return "Birthday{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/fr/todd/zanniv/entity/User.java
@@ -0,0 +1,60 @@
+package fr.todd.zanniv.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="User")
+public class User {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="email")
+    private String email;
+
+    @OneToMany(mappedBy="user")
+    private Set<Birthday> birthdays;
+
+    public User() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Set<Birthday> getBirthdays() {
+        return birthdays;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
